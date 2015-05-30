21e3b77a4fb6b5460fa5e12e1a0929fd50ebc8af
Agustin Marrone <agustinhmarrone@gmail.com>
2015-05-30 02:26:19 -0300

    Herramienta featureInfo funcional. No tiene la capacidad de renderizar links (a tags) retornados por un servidor

--- scripts/tools/featureInfo/directives/FeatureInfoDirective.js
+++ scripts/tools/featureInfo/directives/FeatureInfoDirective.js
@@ -28,42 +28,61 @@ angular.module('visorINTA.tools.featureInfo.FeatureInfoDirective', [])
 	    		}
 	  		});
 
-	  		scope.map.on('singleclick', function(evt) {
-	  			var layersToRequest = [];
-	  			var featureInfoConfig = scope.getFeatureInfoConfig();
-	  			if (featureInfoConfig != "default"){
-	  				for (layerConfig in featureInfoConfig){
-	  					layerObject = $rootScope.getLayerObjectFromConfig(layerConfig);
-	  					layersToRequest.push(layerObject);
-		  			}
-		  			// WARNING -> Hace una peticion independiente por cada capa presente en la config
-		  			//			  del proyecto
-		  			GeoServerUtils.getFeatureInfo(layersToRequest,evt.coordinate,scope.map.getView())
-		  			.then(function success(response) {
-					    scope.bindData(response);
-				    })
-				   	.catch(function error(msg) {
-				       console.error("No se ha podido pedir informacion -- " + msg);
-				   	});
-	  			} else {
-	  				console.log("No se ha configurado informacion para este proyecto");
-	  			}
-	  		})
+	  		console.log(scope.layerList);
+
+			scope.listenEvents = function(){
+				scope.mapListener = scope.map.on('singleclick', function(evt) {
+					scope.updateState(scope.stateList['REQUESTING_DATA']);
+					scope.infoReceived = [];
+		  			var layersToRequest = [];
+		  			var featureInfoConfig = scope.getFeatureInfoConfig();
+		  			if (featureInfoConfig != "default"){
+		  				for (layerConfig in featureInfoConfig){
+		  					layerObject = $rootScope.getLayerObjectFromConfig(layerConfig);
+		  					if (scope.layerIsActive(layerObject)){
+		  						layersToRequest.push(layerObject);
+		  					}
+			  			}
+			  		}
+			  		if (layersToRequest.length > 0 ){
+			  			// WARNING -> Hace una peticion independiente por cada capa presente en la config
+			  			//			  del proyecto
+			  			GeoServerUtils.getFeatureInfo(layersToRequest,evt.coordinate,scope.map.getView())
+			  			.then(function success(response) {
+						    scope.bindData(response);
+					    })
+					   	.catch(function error(msg) {
+					       scope.updateState(scope.stateList['ERROR_DATA']);
+					   	});
+				   } else {
+		  				scope.updateState(scope.stateList['NO_LAYERS']);
+		  				scope.$apply(); // angular no se entera de que cambio el estado sin esta linea
+				   }
+	  			})
+			}
+	  		
 
 			scope.getFeatureInfoConfig = function(){
+				var featureInfoConfig = "default"; // uso el string default, como nulo
 				var model = $rootScope.getProyectModel();
-	  			var pluginConfig = model.modelo[0].toolbar["9"] || null;
-	  			var featureInfoConfig = (pluginConfig) ? pluginConfig.config[0].valor : "default";
+				if (model.modelo){
+	  				var pluginConfig = model.modelo[0].toolbar["9"] || null;
+	  				var featureInfoConfig = (pluginConfig) ? pluginConfig.config[0].valor : "default";
+	  			}
 	  			return featureInfoConfig;
 			}
 
 	        // Acciones a realizar cuando se abre la herramienta
 	        scope.openTool = function(){
-	        	
+	        	if (scope.state != scope.stateList['DATA_RECEIVED']){
+        			scope.updateState();
+	        	}
+	        	scope.listenEvents();
 	        }
 
 	        // Acciones a realizar cuando se cierra la herramienta
 	        scope.closeTool = function(){
+	        	scope.map.unByKey(scope.mapListener);
 	        }
 
 
@@ -76,26 +95,15 @@ angular.module('visorINTA.tools.featureInfo.FeatureInfoDirective', [])
 			$scope.toolName = "featureInfoTool";
 			$scope.toolTitle = "Informacion";
 
-
-			/* Almacena la informacion recibida por peticiones getFeatureInfo.
-			 Es un lista que contiene objectos de la forma:
-			{
-	        			"tabID" : "info_suelos",
-	        			"title" : "Suelos",
-	        			"infoType": "tuple",
-	        			"info": {
-	        				"Field1": "value1",
-	        				"Field2": "value2",
-	        				"FieldN": "valueN",
-	        			}
-    		},
-			Cada objeto, debe guardar informacion de cada capa sobre la cual se pidieron datos
-    		*/
-			$scope.infoReceived = [];
-
+			$scope.updateState = function(config){
+				console.log(config);
+				$scope.state = config || $scope.stateList['TOOL_READY'];
+				console.log($scope.state);
+			}
 
 			// Recibe respuesta getFeatureInfo
 			$scope.bindData = function(data){
+				var existData = false; // flag - existe data en el punto solicitado
 				$scope.infoReceived = []; // limpio info anterior
 				var featureInfoConfig = $scope.getFeatureInfoConfig();
 				for (i in data){ // data contiene respuesta de varias peticiones getFeatureInfo
@@ -105,11 +113,14 @@ angular.module('visorINTA.tools.featureInfo.FeatureInfoDirective', [])
 						layerTitle = layerObjectConfig.get('title');
 						if (layerTitle == infoFeatureResponse.data.layerTitle
 							&& infoFeatureResponse.data.features.length){
+							existData = true;
 							var layerConfig = featureInfoConfig[layerConfig];
 							$scope.bindLayerData(layerTitle,layerConfig,infoFeatureResponse.data.features[0].properties);
 						}
 					}
 				}
+				newState = (existData) ? 'DATA_RECEIVED' : 'NO_DATA';
+				$scope.updateState($scope.stateList[newState]);
 			}
 
 
@@ -129,6 +140,49 @@ angular.module('visorINTA.tools.featureInfo.FeatureInfoDirective', [])
 				}
 				$scope.infoReceived.push(data);
 			}
+
+			$scope.layerIsActive = function(layerObject){
+				for (i in $scope.layerList){
+					lyr = $scope.layerList[i];
+					if (lyr.get('id') == layerObject.get('id')){
+						return true;
+					}
+				}
+				return false;
+			}
+
+
+			$scope.stateList = {
+				'REQUESTING_DATA' : {text:'Pidiendo informacion solicitada...',status:'requesting_data',label:'info'},
+				'DATA_RECEIVED' : {text:'',status:'data_received',label:'info'},
+				'ERROR_DATA': {text:'Ha ocurrido un error mientras se pedia informacion',status:'no_data',label:'danger'},
+				'NO_DATA': {text:'No existe informacion para la coordenada solicitada',status:'no_data',label:'warning'},
+				'NO_LAYERS': {text:'No hay capas activas sobre las cuales pedir informacion',status:'no_layers',label:'info'},
+				'TOOL_READY' : {text:'Haga click en un punto del mapa para recibir informacion',status:'tool_ready',label:'warning'},
+			}
+
+			$scope.updateState($scope.stateList['TOOL_READY']);
+
+			// Almaceno listener a los clicks del mapa
+			$scope.mapListener = null;
+
+
+			/* Almacena la informacion recibida por peticiones getFeatureInfo.
+			 Es un lista que contiene objectos de la forma:
+			{
+	        			"tabID" : "info_suelos",
+	        			"title" : "Suelos",
+	        			"infoType": "tuple",
+	        			"info": {
+	        				"Field1": "value1",
+	        				"Field2": "value2",
+	        				"FieldN": "valueN",
+	        			}
+    		},
+			Cada objeto, debe guardar informacion de cada capa sobre la cual se pidieron datos
+    		*/
+			$scope.infoReceived = [];
+
 
 		}
 	}
